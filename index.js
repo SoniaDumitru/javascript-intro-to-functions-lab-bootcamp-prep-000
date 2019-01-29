cbe4309e228518c42c3f2b981adc75f14456f7f9
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-01-29 18:38:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,19 +2,5 @@ function shout(string) {
   return string.toUpperCase()
 }
 function whisper(string) {
- return string.toLowerCase()
+  return string.toLowerCase()
 }
-function logShout(string) {
-  console.log(string.toUpperCase());
-  logShout("hello");
-}
-// describe('logShout(string)', function() {
-//   it('calls console.log() its one argument in all caps', function() {
-//     const spy = expect.spyOn(console, 'log').andCallThrough()
-//
-//     logShout('hello')
-//
-//     expect(spy).toHaveBeenCalledWith('HELLO')
-//
-//     console.log.restore()
-//   })
